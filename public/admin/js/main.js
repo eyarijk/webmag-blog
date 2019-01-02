38649941a8bb86f5d94e3721e8e64510c7d69496
Yarik <eyarij@gmail.com>
2019-01-02 23:48:06 +0200

    create templates for public and admin panel

---
+++ public/admin/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function($) {
+
+	$('#menuToggle').on('click', function() {
+		$('body').toggleClass('open');
+	});
+
+	$('.search-trigger').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').addClass('open');
+	});
+
+	$('.search-close').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').removeClass('open');
+	});
+
+});
