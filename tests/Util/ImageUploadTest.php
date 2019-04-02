ae4d5c401584b27feff30d154f271770f86fa283
Yarik <eyarij@gmail.com>
2019-04-03 01:00:28 +0300

    unit upload file

---
+++ tests/Util/ImageUploadTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Util;
+
+use App\Service\ImageUpload;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class ImageUploadTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $dir;
+
+    /**
+     * @var UploadedFile
+     */
+    private $image;
+
+    /**
+     * @var string|null
+     */
+    private $path;
+
+    public function setUp()
+    {
+        $this->dir = sys_get_temp_dir();
+
+        $file = tempnam($this->dir, 'upl');
+
+        imagepng(imagecreatetruecolor(10, 10), $file);
+
+        $this->image = new UploadedFile(
+            $file,
+            'image.png',
+            null,
+            null,
+            true
+        );
+    }
+
+    public function testUploadImage(): void
+    {
+        $imageUpload = new ImageUpload($this->dir);
+
+        $fileName = $imageUpload->upload($this->image);
+
+        $this->path = $this->dir . DIRECTORY_SEPARATOR . $fileName;
+
+        $this->assertFileExists($this->path);
+        $this->assertEquals(mime_content_type($this->path),'image/png');
+    }
+
+    public function tearDown(): void
+    {
+        unlink($this->path);
+    }
+}
